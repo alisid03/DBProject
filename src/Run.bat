2105e7f0f456c61a84023a367fde0821ffce2621
Matthew Chimitt <mmchimitt@gmail.com>
2024-04-27 21:58:53 -0500

    fixing some lil issues and also adding capability to avoid putting the mysql-connector in classpath (batch scripts)

---
+++ src/Run.bat
@@ -0,0 +1,3 @@
+@echo off
+javac App.java
+java -cp ".;mysql-connector-j-8.3.0.jar" App
---
+++ src/Setup.bat
@@ -0,0 +1,11 @@
+@echo off
+
+javac CreateDatabases.java
+javac App.java
+javac ./DBManager/Data.java
+javac ./DBManager/DBConnection.java
+javac ./DBManager/HorizontalPartitioningManager.java
+javac ./DBManager/Sharding.java
+javac ./DBManager/Caching.java
+
+java -cp ".;mysql-connector-j-8.3.0.jar" CreateDatabases
